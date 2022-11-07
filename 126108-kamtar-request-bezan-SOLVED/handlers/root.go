4ed622ac8bfdad6ac00b7d178500afd636e9f9be
Saman Mahdanian <mahdanian.saman@gmail.com>
2022-11-07 21:31:42 +0330

    126108 Solved

--- 126108-kamtar-request-bezan-SOLVED/handlers/root.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "net/http"
-
-func Root(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"ok": true}`))
-}
